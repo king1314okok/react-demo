68187f34f37a3bd62f229db931bdd2ed8f8b27c0
oudake <759046694@qq.com>
2020-02-05 23:06:49 +0800

    主页

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserInfo from '@/components/UserInfo'
+
+class Main extends React.Component {
+
+  render () {
+    return (<div>
+      hello, world
+      <UserInfo/>
+    </div>)
+  }
+}
+
+export default Main
---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class UserInfo extends Component {
+  render () {
+    return (
+      <div>This is UserInfo div</div>
+    )
+  }
+}
+
+export default UserInfo
