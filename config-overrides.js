247c7a898fef62846d4934f04140236f5e0ff49a
oudake <759046694@qq.com>
2020-02-06 22:55:28 +0800

    引入node-sass，router，@alias

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,17 @@
 const { override, fixBabelImports } = require('customize-cra')
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
 
 const getConfig = () => config => {
   // 去掉sourcemap
   config.devtool = false
   // 使用eslintrc
   config.module.rules[1].use[0].options.useEslintrc = true
+  // 配置alias
+  config.resolve.alias['@'] = resolve('src')
   return config
 }
 
---
+++ src/pages/Home/containers/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../style.scss'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="main">
+        main page
+      </div>
+    )
+  }
+}
+
+export default Home
