9d72988bf182b9f3bf95cc6663ab91128c8e764c
oudake <759046694@qq.com>
2020-02-06 23:40:34 +0800

    整理项目结构

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Home from './pages/Home/containers/Home'
+import Home from './containers/Home'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render((
